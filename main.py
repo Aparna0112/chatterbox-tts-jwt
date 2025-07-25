ed2cadfe731cc3a34e63e2d2a132d68b7fa1645c
Aparna Padmakumar <159600270+Aparna0112@users.noreply.github.com>
2025-07-25 12:11:13 +0530

    Update main.py

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
 from fastapi.responses import FileResponse, JSONResponse
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel, Field
-import jwt
+import jwt  # This should be PyJWT
 from passlib.context import CryptContext
 import numpy as np
 import soundfile as sf
